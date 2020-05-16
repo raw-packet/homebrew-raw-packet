96e34aa26d2c924e035ee0ee8ff97d8e454979fe
Vladimir Ivanov <ivanov.vladimir.mail@gmail.com>
2020-05-16 09:09:48 -0700

    Update raw-packet.rb

--- raw-packet.rb
+++ raw-packet.rb
@@ -10,11 +10,20 @@ class RawPacket < Formula
   depends_on "wireshark"
   depends_on "nmap"
   
+  resource "raw_packet" do
+    url "https://github.com/raw-packet/raw-packet/archive/v0.2.1.tar.gz"
+    sha256 "87ec91531e1256c1637017602dcd1bb3ed3b752d5abc2bb8d8ad2919ac50234a"
+  end
+  
   def install
+    (libexec/raw_packet).install resource(raw_packet)
     python_version = "3.7"
     site_packages = HOMEBREW_PREFIX/"lib/python#{python_version}/site-packages"
-    system bin/"python3", "setup.py", "install",
-               "--install-scripts=#{bin}",
-               "--install-lib=#{site_packages}"
+    bin_directory = "/usr/local/bin/"
+    (libexec/raw_packet).cd do
+      system "python3", "setup.py", "install",
+             "--install-scripts=#{bin_directory}",
+             "--install-lib=#{site_packages}"
+    end
   end
 end
