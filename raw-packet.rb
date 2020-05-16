126b9e3daf3b50fbaa2978ea9f3868e47ea7ba89
Vladimir Ivanov <ivanov.vladimir.mail@gmail.com>
2020-05-16 08:17:42 -0700

    Update raw-packet.rb

--- raw-packet.rb
+++ raw-packet.rb
@@ -1,9 +1,10 @@
 class RawPacket < Formula
   
-  resource "raw_packet" do
-    url "https://github.com/raw-packet/raw-packet/archive/v0.2.1.tar.gz"
-    sha256 "87ec91531e1256c1637017602dcd1bb3ed3b752d5abc2bb8d8ad2919ac50234a"
-  end
+  desc "This project implements network protocols such as Ethernet ARP IPv4 UDP TCP DHCPv4 ICMPv4 IPv6 DHCPv6 ICMPv6 DNS MDNS on raw socket."
+  homepage "https://github.com/raw-packet/raw-packet/tree/testing"
+  url "https://github.com/raw-packet/raw-packet/archive/v0.2.1.tar.gz"
+  sha256 "87ec91531e1256c1637017602dcd1bb3ed3b752d5abc2bb8d8ad2919ac50234a"
+  head "https://github.com/raw-packet/raw-packet.git"
   
   depends_on "python"
   depends_on "wireshark"
